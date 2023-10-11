51d5411dc086dd189f25976e4ceabad8f754a42f
Bruno Mastropietro <127962081+brunomastro165@users.noreply.github.com>
2023-10-11 17:40:39 -0300

    Delete P1P3 directory

--- P1P3/src/main/java/com/example/springbootapirest/repositories/AutorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springbootapirest.repositories;
-
-import com.example.springbootapirest.entities.Autor;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AutorRepository extends BaseRepository<Autor, Long>{
-
-}
--- P1P3/src/main/java/com/example/springbootapirest/services/LocalidadService.java
+++
@@ -1,8 +0,0 @@
-package com.example.springbootapirest.services;
-
-import com.example.springbootapirest.entities.Localidad;
-
-public interface LocalidadService extends BaseService<Localidad, Long>{
-
-
-}
